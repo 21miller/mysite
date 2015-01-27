672434c75b2abb023a8cf522399fc2cb19d984ad
Hashisx <rmiller805@gmail.com>
2015-01-27 00:32:29 -0800

    c

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
                         <div class="inner-content">
                             <h2>Earth New House Project</h2>
                             <p>Artcore is free HTML5 template by <b class="blue">template</b><b class="green">mo</b>. Credit goes to <a rel="nofollow" href="http://unsplash.com">Unsplash</a> for photos.</p>
-                            <a href="#" class="main-btn white">View Projects</a>
+                            
                         </div> <!-- /.inner-content -->
                     </div> <!-- /.slider-caption -->
                 </div> <!-- /.swier-slide -->
@@ -103,7 +103,7 @@
                         <div class="inner-content">
                             <h2>Hotel and Residence Concept in Montenegro</h2>
                             <p>We come with new fresh and unique ideas.</p>
-                            <a href="#" class="main-btn white">View Projects</a>
+                            
                         </div> <!-- /.inner-content -->
                     </div> <!-- /.slider-caption -->
                 </div> <!-- /.swier-slide -->
@@ -114,7 +114,7 @@
                         <div class="inner-content">
                             <h2>Natural 3d Architecture Design</h2>
                             <p>Natural concrete is a material which is calm and clean.</p>
-                            <a href="#" class="main-btn white">View Projects</a>
+                            
                         </div> <!-- /.inner-content -->
                     </div> <!-- /.slider-caption -->
                 </div> <!-- /.swier-slide -->
