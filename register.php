71d51a9f852e66349c34b115a57c2c80c4c3e842
Hashisx <rmiller805@gmail.com>
2015-01-27 12:19:51 -0800

    c

--- register.php
+++ register.php
@@ -1,125 +1,36 @@
 <?php
-//include config
-require_once('../includes/config.php');
-
-define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : 'forum/';
-
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-include($phpbb_root_path . 'common.' . $phpEx);
-include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
-include($phpbb_root_path . 'includes/functions_user.' . $phpEx);
-
-<!doctype html>
-<html lang="en">
-<head>
-</head>
-<body>
-
-<div id="wrapper">
-
-<h2>Register</h2>
-
-	<?php
-
-	//if form has been submitted process it
-	if(isset($_POST['submit'])){
-
-		//collect form data
-		extract($_POST);
-
-		//very basic validation
-		if($username ==''){
-			$error[] = 'Please enter the username.';
-		}
-
-		if($password ==''){
-			$error[] = 'Please enter the password.';
-		}
-
-		if($passwordConfirm ==''){
-			$error[] = 'Please confirm the password.';
-		}
-
-		if($password != $passwordConfirm){
-			$error[] = 'Passwords do not match.';
-		}
-
-		if($email ==''){
-			$error[] = 'Please enter the email address.';
-		}
-
-		if($api_id ==''){
-			$error[] = 'Please enter the API Id.';
-		}
-
-		if($api_key ==''){
-			$error[] = 'Please enter the API Key.';
-		}
-
-		if(!isset($error)){
-
-			$hashedpassword = $user->create_hash($password);
-
-			try {
-
-				//insert into database
-				$stmt = $db->prepare('INSERT INTO phpbb_users (username,user_password,user_email) VALUES (:username, :password, :email)') ;
-				$stmt->execute(array(
-					':username' => $username,
-					':password' => $hashedpassword,
-					':email' => $email
-				));
-
-				$stmt = $db->prepare('INSERT INTO phpbb_profile_fields_data (pf_api_id,pf_api_key) VALUES (:api_id, :api_key)') ;
-				$stmt->execute(array(
-					':api_id' => $api_id,
-					':api_key' => $api_key,
-				));
-
-				//redirect to index page
-				header('Location: index.php');
-				exit;
-
-			} catch(PDOException $e) {
-			    echo $e->getMessage();
-			}
-
-		}
-
-	}
-
-	//check for any errors
-	if(isset($error)){
-		foreach($error as $error){
-			echo '<p class="error">'.$error.'</p>';
-		}
-	}
-	?>
-
-	<form action='' method='post'>
-
-		<p><label>API ID</label><br />
-		<input type='text' name='api_id' value='<?php if(isset($error)){ echo $_POST['api_id'];}?>'></p>
-
-		<p><label>API Key</label><br />
-		<input type='text' name='api_key' value='<?php if(isset($error)){ echo $_POST['api_key'];}?>'></p>
-
-		<p><label>Username</label><br />
-		<input type='text' name='username' value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
-
-		<p><label>Password</label><br />
-		<input type='password' name='password' value='<?php if(isset($error)){ echo $_POST['password'];}?>'></p>
-
-		<p><label>Confirm Password</label><br />
-		<input type='password' name='passwordConfirm' value='<?php if(isset($error)){ echo $_POST['passwordConfirm'];}?>'></p>
-
-		<p><label>Email</label><br />
-		<input type='text' name='email' value='<?php if(isset($error)){ echo $_POST['email'];}?>'></p>
-		
-		<p><input type='submit' name='submit' value='Register'></p>
-
-	</form>
-
-</div>
-
+//retrieve our data from POST
+$username = $_POST['username'];
+$password1 = $_POST['password1'];
+$password2 = $_POST['password2'];
+$email = $_POST['email'];
+ 
+if($password1 != $password2)
+    header('Location: registration.php');
+ 
+if(strlen($username) > 30)
+    header('Location: registration.php');
+$hash = hash('sha256', $password1);
+ 
+function createSalt()
+{
+    $text = md5(uniqid(rand(), true));
+    return substr($text, 0, 3);
+}
+ 
+$salt = createSalt();
+$password = hash('sha256', $salt . $hash);
+$conn = mysql_connect('localhost', 'Hashisx', 'd4gBreak1987!');
+mysql_select_db('login', $conn);
+ 
+//sanitize username
+$username = mysql_real_escape_string($username);
+ 
+$query = "INSERT INTO phpbb.phpbb_users ( username, user_password, user_email, user_form_salt )
+        VALUES ( '$username', '$password', '$email', '$salt' );";
+mysql_query($query);
+ 
+mysql_close();
+ 
+header('Location: index.php');
+?>
---
+++ registration.php
@@ -0,0 +1,31 @@
+
+<form name="register" action="register.php" method="post">
+	<table width="510" border="0">
+		<tr>
+			<td colspan="2"><p><strong>Registration Form</strong></p></td>
+		</tr>
+		<tr>
+			<td>Username:</td>
+			<td><input type="text" name="username" maxlength="20" /></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="password1" /></td>
+		</tr>
+		<tr>
+			<td>Confirm Password:</td>
+			<td><input type="password" name="password2" /></td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td><input type="text" name="email" id="email" /></td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" value="Register" /></td>
+		</tr>
+	</table>
+</form>
+
+</div>
+
