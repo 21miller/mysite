f63be90ede09fd640a9f09062ca9238126de558a
Hashisx <rmiller805@gmail.com>
2015-01-28 12:33:13 -0800

    c

--- login.php
+++ login.php
@@ -31,8 +31,8 @@ echo('Log In<br><form action="forum/ucp.php" method="post" enctype="multipart/fo
 <label for="password">Password:</label><input type="password" name="password" /><br />
 <input type="hidden" name="redirect" value="../index.php" /><br />
 <label for="username">Automatic login:</label><input type="checkbox" name="autologin" id="autologin" class="checkbox" /><br />
-<input type="submit" value="login" name="Login" />
-<input type="submit" value="register.php" name="Register" />
+<input type="submit" value="Login" name="Login" />
+<input type="submit" action="register.php" value="Register" name="Register" />
 </form>');
 } ?>
 
