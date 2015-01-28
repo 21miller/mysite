9e8d5d6c46cff32f6b97c52e724e32ed291c0fdb
21miller <rmiller805@gmail.com>
2015-01-28 18:34:37 +0000

    master

--- register.php
+++ register.php
@@ -5,30 +5,20 @@ $phpbb_root_path = 'forum/';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 
-// Start session management
-$user->session_begin();
-$auth->acl($user->data);
-$user->setup(); ?>
+?>
 
-<?php
-if($user->data['is_registered'])
-{
-echo("<b>Hi " . $user->data['username'] . "!</b><br /><br /><a href='loggedout.php'>Logout</a>" );
-}
-else
-{
-echo(<form name="register" action="forum/includes/ucp/ucp_register.php" method="post" enctype="multipart/form-data">
+<form name="register" action="add_user.php" method="post" enctype="multipart/form-data">
 	<table width="510" border="0">
 		<tr>
 			<td colspan="2"><p><strong>Registration Form</strong></p></td>
 		</tr>
 		<tr>
 			<td>Username:</td>
-			<td><input type="text" name="username" maxlength="20" /></td>
+			<td><input type="text" name="f_username" maxlength="20" /></td>
 		</tr>
 		<tr>
 			<td>Password:</td>
-			<td><input type="password" name="new_password" /></td>
+			<td><input type="password" name="f_password" /></td>
 		</tr>
 		<tr>
 			<td>Confirm Password:</td>
@@ -36,7 +26,7 @@ echo(<form name="register" action="forum/includes/ucp/ucp_register.php" method="
 		</tr>
 		<tr>
 			<td>Email:</td>
-			<td><input type="text" name="email" id="email" /></td>
+			<td><input type="text" name="f_email" id="email" /></td>
 		</tr>
 		<tr>
 			<td>&nbsp;</td>
@@ -44,7 +34,4 @@ echo(<form name="register" action="forum/includes/ucp/ucp_register.php" method="
 			<input type="hidden" name="redirect" value="../index.php" /><br />
 		</tr>
 	</table>
-</form>);
-?>
-</div>
-
+</form>
